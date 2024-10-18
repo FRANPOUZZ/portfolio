11fe1da148e60ee4380dd7d71cfdfe7214f3beff
FRANPOUZZ <francois.croizer64@gmail.com>
2024-10-18 14:17:38 +0200

    commit du dossier de projet modifié

--- portfolio/src/components/presentation.vue
+++ portfolio/src/components/presentation.vue
@@ -4,8 +4,8 @@
         <div>
             <h2>Présentation</h2>
             <p>François Croizer</p>
-            <p>Développeur web débutant de <br>22 ans qui étudie avec le centre européen de formation.</p>
-            <p><strong>Languages maitrisés: </strong>HTML,CSS,Javascript <br></p>
+            <p>Développeur web débutant <br>qui étudie avec le centre européen de formation.</p>
+            <p><strong>Téchnologies maitrisés: </strong>HTML, CSS, Javascript, Git/github, Vue.js. <br></p>
         </div>
     </section>
 </body>
